91eba3ff941d3ab164ba904686727e7b830d46b3
MadSellie <samu_9@mail.ru>
2022-04-12 22:10:59 +0600

    initial commit

---
+++ src/reducers/heroes.js
@@ -0,0 +1,49 @@
+const initialState = {
+	heroes: [],
+	heroesLoadingStatus: 'idle',
+}
+
+const heroes = (state = initialState, action) => {
+	switch (action.type) {
+		case 'HEROES_FETCHING':
+			return {
+				...state,
+				heroesLoadingStatus: 'loading'
+			}
+		case 'HEROES_FETCHED':
+			return {
+				...state,
+				heroes: action.payload,
+				/*filteredHeroes: state.activeFilter === 'all' ?
+								action.payload :
+								action.payload.filter(item => item.element === state.activeFilter),*/
+				heroesLoadingStatus: 'idle'
+			}
+		case 'HEROES_FETCHING_ERROR':
+			return {
+				...state,
+				heroesLoadingStatus: 'error'
+			}
+		case 'HERO_CREATED':
+			// let newCreatedHeroList = [...state.heroes, action.payload];
+			return {
+				...state,
+				heroes: [...state.heroes, action.payload],
+				/*filteredHeroes: state.activeFilter === 'all' ?
+								newCreatedHeroList :
+								newCreatedHeroList.filter(item => item.element === state.activeFilter)*/
+			}
+		case 'HERO_DELETED':
+			// const newHeroList = state.heroes.filter(item => item.id !== action.payload);
+			return {
+				...state,
+				heroes: state.heroes.filter(item => item.id !== action.payload),
+				/*filteredHeroes: state.activeFilter === 'all' ?
+								newHeroList :
+								newHeroList.filter(item => item.element === state.activeFilter)*/
+			}
+		default: return state
+	}
+}
+
+export default heroes;
